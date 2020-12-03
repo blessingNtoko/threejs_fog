037930eba5f2ebded9a2d91d24dfd3bc3dfd689e
Blessing <bntoko@gmail.com>
2020-12-03 14:45:22 +0200

    basic setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'threejsFog';
+export class AppComponent implements OnInit {
+
+  public scene = new THREE.Scene();
+  public renderer = new THREE.WebGLRenderer({
+    antialias: true
+  });
+  public camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, .1, 1000);
+  public controls = new OrbitControls(this.camera, this.renderer.domElement);
+
+  ngOnInit() {
+    this.init();
+  }
+
+  public init() {
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.renderer.domElement);
+
+    this.camera.position.set(10, 0, 0);
+
+
+    const animate = () => {
+
+      this.renderer.render(this.scene, this.camera);
+      requestAnimationFrame(animate);
+    }
+    requestAnimationFrame(animate);
+
+  }
 }
